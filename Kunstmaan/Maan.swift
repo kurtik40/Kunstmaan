b858236f0408ae977c34a28f6adf5d95c813f481
Kurt Warson <kurtwarson@kurts-mac-mini.local>
2018-07-06 13:07:03 +0200

    Toevoeging class Maan

---
+++ Kunstmaan/Maan.swift
@@ -0,0 +1,33 @@
+//
+//  Hero.swift
+//  CustomTableView
+//
+//  Created by Belal Khan on 30/07/17.
+//  Copyright © 2017 Belal Khan. All rights reserved.
+//
+
+class Maan {
+    
+    //    var name: String?
+    //    var team: String?
+    //    var imageUrl: String?
+    //
+    //    init(name: String?, team: String?, imageUrl: String?) {
+    //        self.name = name
+    //        self.team = team
+    //        self.imageUrl = imageUrl
+    
+    var beschrijving: String?
+    var beschrijving2: String?
+    var imageUrl1: String?
+    var imageUrl2: String?
+   
+    
+    init(beschrijving: String?,beschrijving2:String?, imageUrl1: String?, imageUrl2: String?, imageUrl3: String?) {
+        self.beschrijving = beschrijving
+        self.beschrijving2 = beschrijving2
+        self.imageUrl1 = imageUrl1
+        self.imageUrl2 = imageUrl2
+                }
+    
+}
